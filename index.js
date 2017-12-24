25c945fb3e8b8a2d23fc369cbc5ecdf82e7534f9
Daniel Kempner <djkempner@gmail.com>
2017-12-24 14:37:42 -0800

    momenting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Agenda = require('agenda');
-const app = express();
 const FitbitApiClient = require('fitbit-node');
+const moment = require('moment');
+
+const app = express();
 const redirectUriBase = 'http://localhost:3000/';
 const fitbitRedirect = redirectUriBase + 'fitbit-auth-response';
 
-
+var currentAccessToken = '';
 
 var fitbitApiClient = new FitbitApiClient('22CH7N', '2312d65226b5710672fdff7ebe8d1e17');
 
@@ -34,34 +36,30 @@ app.get('/fitbit-auth-response', (req, res) => {
     var authCode = req.query.code;
     var getAccessToken = getFitbitAccessToken(authCode);
     getAccessToken.then((result) => {
-        var token = result.access_token;
-        getCurrentHeartRate(token).then((result) => {
+        currentAccessToken = result.access_token;
+        getCurrentHeartRate(currentAccessToken).then((result) => {
             res.send(result[0]['activities-heart-intraday']['dataset']);
         });
     });
 });
 app.listen('3000');
 
-
-
-
-
-
-
-
-
-
 // agenda is background job processing
 const agendaConnectionString = 'mongodb://localhost:27017/agenda';
 var agenda = new Agenda({ db: { address: agendaConnectionString } });
 
 
-agenda.define('log', () => {
-
+agenda.define('refresh-heartrate', (job, done) => {
+    console.log('doing it');
+    // done();
+    // getCurrentHeartRate(currentAccessToken).then(result => {
+    //     debugger;
+    //     done();
+    // });
 });
 
 agenda.on('ready', () => {
-    agenda.every('10 seconds', 'log');
+    agenda.every('2 seconds', 'refresh-heartrate');
     agenda.start();
 });
 
